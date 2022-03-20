b81882a25555ed701e7e520ae0874f7e7dd17fa7
xiexuan-star <xiexuan127@foxmail.com>
2022-03-20 17:06:03 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div xe:40="h bg-primary hover:bg-primary"></div>
   <div xe:100="w h" xe-bg-red-80 xe-flex="~ jc-center ai-center">22</div>
-  <div xe:30="w h" :xe-fc-black='isActive?"75":"45"'>
+  <div xe:30="w h" :xe-fc-black='isActive?"75":"55"'>
     font
   </div>
   <button @click="isActive=!isActive">click</button>
